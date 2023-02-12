2585c197b7b215a786302c9b0ab40a05d2c9919c
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-02-12 18:04:52 +0700

    Fix directories hierarchy

--- JavaScript first steps/script/script.js
+++
@@ -1,6 +0,0 @@
-const playerBox = document.querySelector('p');
-
-playerBox.addEventListener('click', () => {
-    const name = prompt('Enter a new name');
-    playerBox.textContent = `Player 1: ${name}`;
-});
